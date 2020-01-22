75b66bc9b468287bbdc0a2f81387566374ad0d13
Mosaab Emam <mosaab.emam123@gmail.com>
2020-01-22 10:36:02 +0200

    Added loading screen

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-document.querySelectorAll(".accordion-btn").forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("active"),t.nextElementSibling.classList.toggle("active")})});
+window.onload=function(){document.getElementById("loading-left").style.width="0",document.getElementById("loading-right").style.width="0",document.getElementById("loading-spinner").style.opacity="0"},document.querySelectorAll(".accordion-btn").forEach(function(t){t.addEventListener("click",function(){t.classList.toggle("active"),t.nextElementSibling.classList.toggle("active")})});

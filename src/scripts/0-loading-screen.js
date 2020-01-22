43b78ac11df0802cf25e2432acd3a96bb17c61ba
Mosaab Emam <mosaab.emam123@gmail.com>
2020-01-22 10:36:02 +0200

    Added loading screen

---
+++ src/scripts/0-loading-screen.js
@@ -0,0 +1,6 @@
+// Loading screen
+window.onload = function() {
+  document.getElementById("loading-left").style.width = "0";
+  document.getElementById("loading-right").style.width = "0";
+  document.getElementById("loading-spinner").style.opacity = "0";
+};
